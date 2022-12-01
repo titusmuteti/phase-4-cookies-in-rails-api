78fb6b96e21c4512d183ba68013af52a18d4c908
Titus Mumo <titusmumo@gmail.com>
2022-12-01 11:35:23 +0300

    Done

--- config/application.rb
+++ config/application.rb
@@ -44,4 +44,20 @@ module Phase4CookiesInRailsApi
     # Use SameSite=Strict for all cookies to help protect against CSRF
     config.action_dispatch.cookies_same_site_protection = :strict
   end
+
+  module MyApp
+    class Application < Rails::Application
+      config.load_defaults 6.1
+      # This is set in apps generated with the --api flag, and removes session/cookie middleware
+      config.api_only = true
+  
+      # Must add these lines!
+      # Adding back cookies and session middleware
+      config.middleware.use ActionDispatch::Cookies
+      config.middleware.use ActionDispatch::Session::CookieStore
+  
+      # Use SameSite=Strict for all cookies to help protect against CSRF
+      config.action_dispatch.cookies_same_site_protection = :strict
+    end
+  end
 end
